3833e5dd5c1e7a24508086f3ad920b7c69763136
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2021-12-06 23:25:14 -0300

    solve some sorries in degree_new.lean

--- src/degree_new.lean
+++ src/degree_new.lean
@@ -39,16 +39,42 @@ def max : multiset ℕ  → ℕ :=
 multiset.foldr (max) (λ x y z, by simp [max_left_comm]) 0
 -/
 
+-- this def is also given in flt-regular
 def monomial_degree {s : Type} (t : s →₀ ℕ) : ℕ := t.sum (λ _ e, e) --∑ i in t.support, t i
+
+lemma nat.term_le_sum {s : finset α } (f : α → ℕ){j : α} (hj : j ∈ s) : f j ≤ s.sum f :=
+begin
+  revert j,
+  apply finset.cons_induction_on s,
+  simp,
+  clear s,
+  intros x s hx hj j hc,
+  rw finset.sum_cons,
+  simp only [finset.mem_cons] at hc,
+  cases hc with j_eq_x j_in_s,
+  simp [j_eq_x],
+  apply (hj j_in_s).trans,
+  simp,
+end
 
 lemma le_monomial_degree  {s : Type} (t : s →₀ ℕ) (j : s) : t j ≤ monomial_degree t :=
 begin
-  sorry
+  by_cases c : j ∈ t.support,
+  { apply nat.term_le_sum,
+    exact c },
+  simp only [not_not, finsupp.mem_support_iff] at c,
+  simp [c],
 end
 
+
 lemma monomial_degree_sub  {σ : Type*} {m m' :  σ →₀ ℕ} (h : m' ≤ m) : 
-  monomial_degree (m-m') = monomial_degree m - monomial_degree m' := 
+  monomial_degree (m - m') = monomial_degree m - monomial_degree m' := 
 begin
+  simp [monomial_degree],
+  rw finsupp.sum,
+  simp,
+  have t:= @finset.sum_sub_distrib ℕ σ (m-m').support m m',
+  apply (int.coe_nat_eq_coe_nat_iff _ _).1,
   sorry
 end
 
@@ -59,8 +85,10 @@ total_degree (monomial m a) = monomial_degree m := by  convert total_degree_mono
 -- Use monomial instead of single!
 lemma monomial_degree_single {σ : Type*} {j : σ} {d : ℕ}:
 monomial_degree (single j d) = d :=
+--monomial_degree (X j)^d = d :=
 begin
-  sorry
+  rw monomial_degree,
+  simp,
 end
 
 lemma monomial_degree_le_total_degree {σ R : Type*}[comm_semiring R] {m :  σ →₀ ℕ} {f  : mv_polynomial σ R} 
@@ -73,7 +101,12 @@ lemma coeff_zero_of_degree_greater_than_total_degree { n : ℕ } {F : Type u} [f
 (t : fin n →₀ ℕ) (f : mv_polynomial (fin n) F) :
 monomial_degree t > total_degree f → coeff t f = 0 :=
 begin
-  sorry
+  intro h,
+  by_cases c: t ∈ f.support,
+  { exfalso,
+    simpa using lt_of_le_of_lt (monomial_degree_le_total_degree c) h },
+  simp only [not_not, mem_support_iff] at c,
+  exact c,
 end
 
 /-
