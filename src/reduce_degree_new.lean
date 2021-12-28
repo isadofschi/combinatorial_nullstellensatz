49401868f56a9aa886415aa90d0c2bb5d9577933
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2021-12-28 00:33:07 -0300

    prove more lemmas

--- src/reduce_degree_new.lean
+++ src/reduce_degree_new.lean
@@ -43,12 +43,67 @@ open_locale big_operators
 namespace mv_polynomial
 open set function finsupp add_monoid_algebra
 
+local attribute [instance] classical.prop_decidable
+
+lemma induction_on_total_degree {R σ : Type*} [comm_semiring R] {M : mv_polynomial σ R → Prop}
+ (p : mv_polynomial σ R) (h : ∀ (p' : mv_polynomial σ R),
+   (∀ q,  total_degree q < total_degree p' → M q) → M p') : M p :=
+begin
+  let P : ℕ → Prop := λ n, ∀ p : mv_polynomial σ R, total_degree p ≤ n → M p,
+  suffices l' : ∀ n, P n,
+  { apply l' (total_degree p),
+    refl },
+  { intro n,
+    induction n with d hd,
+    { intros p hp,
+      apply h p,
+      intros q hq,
+      simpa using lt_of_lt_of_le hq hp },
+    { intros p hp,
+      apply h p,
+      intros q hq,
+      exact hd q (nat.le_of_lt_succ (lt_of_lt_of_le hq hp)) } },
+end
+
 lemma induction_on_new {R σ : Type*} [comm_semiring R] {M : mv_polynomial σ R → Prop} (p : mv_polynomial σ R)
   (h_add_weak : ∀ (a : σ →₀ ℕ) (b : R) (f : (σ →₀ ℕ) →₀ R),
     a ∉ f.support → b ≠ 0 → M f → M (monomial a b) → M (monomial a b + f))
   (h_monomial : ∀ m : σ →₀ ℕ, ∀ b : R,
     (∀ p : mv_polynomial σ R, total_degree p < monomial_degree m → M p) → M (monomial m b)) 
-  : M p := sorry
+  : M p :=
+  begin
+    apply induction_on_total_degree,
+    { intros p,
+      apply induction_on''' p,
+      { intros a h,
+        apply h_monomial,
+        intros x h2,
+        rw monomial_degree at h2,
+        simpa using h2 },
+      { intros a b f ha hb hMf h,
+        apply h_add_weak a b f ha hb (hMf _),
+        { apply h_monomial,
+          intros p' hp',
+          suffices h' : p'.total_degree < (monomial a b).total_degree,
+          { apply h p',
+            apply lt_of_lt_of_le h',
+            rw total_degree_add_eq_of_disjoint_support,
+            { simp only [le_refl, true_or, le_max_iff] },
+            { simp only [ support_monomial, hb, not_not, mem_support_iff, if_false, 
+                          finset.disjoint_singleton_left],
+              simpa using ha } },
+          rw total_degree_monomial_eq_monomial_degree,
+          { exact hp' },
+          { exact hb } },
+        { intros q hq,
+          apply (h q) ∘ (lt_of_lt_of_le hq),
+          rw total_degree_add_eq_of_disjoint_support,
+          { simp only [le_refl, or_true, le_max_iff] },
+          { simp only [support_monomial, hb, not_not, mem_support_iff, if_false, 
+                     finset.disjoint_singleton_left],
+            simpa using ha } } } },
+  end
+  
 
 
 def is_reduced {R σ : Type*} [comm_ring R] (f : mv_polynomial σ R) (m : σ →₀ ℕ) : Prop
@@ -130,7 +185,7 @@ begin
     exact is_reduced_add (h_hab.2 i) (h_hf.2 i) }
 end
 
-local attribute [instance] classical.prop_decidable
+
 
 lemma total_degree_sub_lt {R σ : Type*} [comm_ring R] [is_domain R] 
 {f g : mv_polynomial σ R} {k : ℕ} (h : 0 < k)
@@ -141,12 +196,94 @@ begin
   sorry,
 end
 
-lemma dominant_monomial_iff {R σ : Type*} [comm_ring R] [is_domain R] 
+lemma total_degree_eq' {R σ : Type*} [comm_semiring R] (p : mv_polynomial σ R) :
+  p.total_degree = p.support.sup (monomial_degree) :=
+begin
+  rw [total_degree],
+  congr, funext m,
+end
+
+lemma max_degree_monomial_iff_of_eq_degree' {R σ : Type*} [comm_semiring R] (p : mv_polynomial σ R)
+ {m m' : σ →₀ ℕ} (hm' : m' ∈ p.support) (h : monomial_degree m = monomial_degree m' ) : 
+ max_degree_monomial m p → max_degree_monomial m' p :=
+begin
+  intro h',
+  split,
+  { exact hm' },
+  { rw ← h,
+    exact h'.2 }
+ end
+
+lemma max_degree_monomial_iff_of_eq_degree {R σ : Type*} [comm_semiring R] (p : mv_polynomial σ R)
+ {m m' : σ →₀ ℕ} (hm : m ∈ p.support) (hm' : m' ∈ p.support) (h : monomial_degree m = monomial_degree m') : 
+ max_degree_monomial m p ↔ max_degree_monomial m' p :=
+begin
+  split,
+  { apply max_degree_monomial_iff_of_eq_degree',
+    { exact hm' },
+    { exact h } },
+  { apply max_degree_monomial_iff_of_eq_degree',
+    { exact hm },
+    { exact h.symm } }
+ end
+
+
+lemma max_degree_monomial_iff {R σ : Type*} [comm_ring R]
 {f : mv_polynomial σ R} { m : σ →₀ ℕ} :
-dominant_monomial m f ↔ ∀ m' ∈ f.support, 
-  monomial_degree m' ≤ monomial_degree m ∧ (monomial_degree m' = monomial_degree m → m = m')
-:= begin
-  sorry
+max_degree_monomial m f ↔ m ∈ f.support ∧ ∀ m' ∈ f.support, 
+  monomial_degree m' ≤ monomial_degree m :=
+begin
+  split,
+  { intro h,
+    split,
+    { exact h.1 },
+    { intros m' hm',
+      have t := h.2,
+      rw total_degree_eq' at t,
+      rw t,
+      apply finset.le_sup hm' } },
+  { intro h,
+    split,
+    { exact h.1 },
+    { rw total_degree_eq',
+      rw ← finset.sup'_eq_sup,
+      { apply le_antisymm,
+        { apply finset.le_sup',
+          exact h.1 },
+        { apply finset.sup'_le,
+          exact h.2 } } } },
+end
+
+
+lemma dominant_monomial_iff {R σ : Type*} [comm_ring R]
+{f : mv_polynomial σ R} { m : σ →₀ ℕ} :
+dominant_monomial m f → ∀ m' ∈ f.support, 
+  monomial_degree m' ≤ monomial_degree m ∧ (monomial_degree m' = monomial_degree m → m' = m) :=
+begin
+  --split,
+  { intros h m' hm',
+    split,
+    { apply (max_degree_monomial_iff.1 h.1).2,
+      exact hm' },
+    { intro h1,
+      apply h.2,
+      rw max_degree_monomial_iff_of_eq_degree f hm' h.1.1 h1,
+      exact h.1} },
+  /-
+  { intro h,
+    have hm : max_degree_monomial m f,
+    { apply max_degree_monomial_iff.2,
+      split,
+      have h := exists_max_degree_monomial,
+      intros m' hm',
+      exact ((h m') hm').1 },
+    split,
+    { exact hm },
+    { intros t' ht',
+      apply (h t' ht'.1).2,
+      rw ht'.2,
+      symmetry,
+      rw hm.2 } }-/
 end
 
 private lemma total_degree_p_aux_2 { σ : Type*} { m m' a: σ →₀ ℕ}
@@ -160,8 +297,6 @@ begin
     exact c i },
   { exact h_m_le_a i }
 end
-
-
 
 private lemma total_degree_p_aux_1 { σ : Type*} { m m' a: σ →₀ ℕ}
   (h_m_le_a : m ≤ a) (c : a - m ≤ m'):  m' - (a - m) = m + m' - a :=
@@ -179,8 +314,6 @@ begin
       { exact h_m_le_a i } },
     { exact c i },
 end
-
-
 
 private lemma total_degree_p_aux { σ : Type*} { m m' a: σ →₀ ℕ}
 (h_m_le_a : m ≤ a) (h : monomial_degree a ≤ monomial_degree m')
@@ -269,12 +402,16 @@ begin
         ext x,
         simp only [coe_tsub, pi.sub_apply],
         rw nat.sub_sub_self (h_m_le_a x) },
-      rw dominant_monomial_iff at hm,
       have t0 := hm'.2,
       rw ← ne.def at t0,
       rw ← mem_support_iff at t0,
-      have t1 := hm (m' - (a - m)) t0,
-      apply total_degree_p_aux h_m_le_a h c t1 },
+      have t1 := (dominant_monomial_iff hm) (m' - (a - m)) t0,
+      apply total_degree_p_aux h_m_le_a h c,
+      split,
+      { exact t1.1 },
+      { intro h,
+        symmetry,
+        exact t1.2 h } },
     { simp only [c, eq_self_iff_true, not_true, if_false] at hm',
       simpa using hm' } }
 end
